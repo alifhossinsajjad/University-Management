696262bf967a122dd4f989d376ce23bf63de0e91
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2026-02-02 23:11:49 +0600

    create reuseable function in for nav items

--- src/routes/adminRoutes.tsx
+++ src/routes/adminRoutes.tsx
@@ -3,16 +3,18 @@ import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
-
+import { NavLink } from "react-router";
 
 type TRoute = {
-path : string,
-element : ReactNode
-}
+  path: string;
+  element: ReactNode;
+};
 
-
-
-
+type TAdminSidebarItem = {
+  key: string;
+  label: ReactNode;
+  children?: TAdminSidebarItem[];
+};
 
 const adminPaths = [
   {
@@ -42,9 +44,31 @@ const adminPaths = [
   },
 ];
 
+export const adminSidebarItems = adminPaths.reduce(
+  (acc: TAdminSidebarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+    if (item.Children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.Children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{item.name}</NavLink>,
+        })),
+      });
+    }
+    return acc ;
+  }, []
+);
+
 //programetical way
 
-export const adminRoutes = adminPaths.reduce((acc : TRoute[], item) => {
+export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
   if (item.path && item.element) {
     acc.push({
       path: item.path,
@@ -60,7 +84,7 @@ export const adminRoutes = adminPaths.reduce((acc : TRoute[], item) => {
       });
     });
   }
-  return acc
+  return acc;
 }, []);
 
 //! hard code way
