c77ae813136028fdef28b6ebc65ab2b98f13c37b
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2026-02-14 22:43:02 +0600

    added serialchek for ingnore the non serial object

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import authReducer from './feature/auth/authSlice'
 import { baseApi } from "./api/baseApi";
+import { persistReducer, persistStore, FLUSH, REHYDRATE, REGISTER, PAUSE, PERSIST, PURGE, } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+    key: 'auth',
+    storage,
+}
+
+const persistAuthReducer = persistReducer(persistConfig, authReducer)
 
 const store = configureStore({
     reducer: {
         [baseApi.reducerPath]: baseApi.reducer,
-        auth: authReducer
+        auth: persistAuthReducer
     },
     middleware: (getDefaultMiddlewares) =>
-        getDefaultMiddlewares().concat(baseApi.middleware)
+        getDefaultMiddlewares({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, REGISTER, PAUSE, PERSIST, PURGE,]
+            }
+        }).concat(baseApi.middleware)
 
 })
 
@@ -21,3 +32,5 @@ export default store
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+export const persistor = persistStore(store)
